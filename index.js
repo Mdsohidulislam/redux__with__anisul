de768660973285af3e4211b972defee8937f40dc
Mdsohidulislam <mddsohidulislam@gmail.com>
2022-11-09 17:21:32 -0800

    review lesson 5 with anisul

--- index.js
+++ index.js
@@ -1,76 +1,143 @@
+// const { createStore } = require("redux");
+
 const { createStore } = require("redux");
 
-/// defining constant
+// /// defining constant
+// const INCREMENT = 'INCREMENT';
+// const DECREMENT = 'DECREMENT';
+// const ADD_USER = 'ADD_USER'
+
+
+// /// state defining
+// const initialCounterState = {
+//     count: 0,
+// }
+
+// const initialUserState = {
+//     users: [{name: 'Md Sohidul Islam'}]
+// }
+
+
+// // action type=== object => payload, type
+
+// const incrementCounter = () => {
+//     return {
+//         type: INCREMENT
+//     }
+// }
+
+// const decrementCounter = () => {
+//     return {
+//         type: DECREMENT
+//     }
+// }
+
+// // const addUser = (user) => {
+// //     return {
+// //         type: ADD_USER,
+// //         payload: user
+// //     }
+// // }
+
+
+// // reducer 
+
+// const counterReducer = (state=initialCounterState, action) => {
+//     switch (action.type) {
+//         case INCREMENT:
+//             return{
+//                 ...state,
+//                 count: state.count + 1
+//             }  
+//         case DECREMENT:
+//             return{
+//                 ...state,
+//                 count: state.count - 1
+//             }  
+//         default:
+//             state;
+//     }
+// }
+
+// const store = createStore(counterReducer);
+// store.subscribe(()=>{ 
+//     console.log(store.getState());
+// })
+// store.dispatch(incrementCounter());
+// store.dispatch(incrementCounter());
+// store.dispatch(incrementCounter());
+// store.dispatch(incrementCounter());
+// store.dispatch(incrementCounter());
+// store.dispatch(incrementCounter());
+
+
+// /// summery of redux
+// // state 
+// // dispatch action
+// // reducer
+// // store update
+
+// redux 4 thing remember
+// state
+// action
+// reducer
+// store
+
+// constant defining
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
-const ADD_USER = 'ADD_USER'
+const RESET = 'RESET';
 
-
-/// state defining
-const initialCounterState = {
-    count: 0,
+const initialCount = {
+    count: 0
 }
 
-const initialUserState = {
-    users: [{name: 'Md Sohidul Islam'}]
-}
-
-
-// action type=== object => payload, type
-
-const incrementCounter = () => {
+const incrementCount = () => {
     return {
-        type: INCREMENT
+        type: INCREMENT,
     }
 }
 
-const decrementCounter = () => {
+const decrementCount = () => {
     return {
         type: DECREMENT
     }
 }
 
-// const addUser = (user) => {
-//     return {
-//         type: ADD_USER,
-//         payload: user
-//     }
-// }
-
-
-// reducer 
-
-const counterReducer = (state=initialCounterState, action) => {
-    switch (action.type) {
-        case INCREMENT:
-            return{
-                ...state,
-                count: state.count + 1
-            }  
-        case DECREMENT:
-            return{
-                ...state,
-                count: state.count - 1
-            }  
-        default:
-            state;
+const resetCount = () => {
+    return {
+        type: RESET
     }
 }
 
+const counterReducer = (state=initialCount, action) => {
+    switch(action.type){
+        case INCREMENT:
+            return {
+                ... state,
+                count: state.count+1
+            }
+        case DECREMENT:
+            return {
+                ... state,
+                count: state.count - 1
+            }  
+        case RESET:
+            return {
+                ... state,
+                count:  0
+            }  
+    }
+};
+
 const store = createStore(counterReducer);
-store.subscribe(()=>{ 
+
+store.subscribe(()=>{
     console.log(store.getState());
 })
-store.dispatch(incrementCounter());
-store.dispatch(incrementCounter());
-store.dispatch(incrementCounter());
-store.dispatch(incrementCounter());
-store.dispatch(incrementCounter());
-store.dispatch(incrementCounter());
 
-
-/// summery of redux
-// state 
-// dispatch action
-// reducer
-// store update
+store.dispatch(incrementCount())
+store.dispatch(incrementCount())
+store.dispatch(incrementCount())
+store.dispatch(decrementCount())
+store.dispatch(resetCount())
